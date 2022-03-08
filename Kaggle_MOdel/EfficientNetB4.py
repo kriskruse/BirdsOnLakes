bd6279b06d0266f41fa3ca63a6fd2e5636345191
Kris Kruse <kriskruse@hotmail.com>
2022-03-08 11:57:21 +0100

    Efficientnet 2
    So the data is imported and converted correctly. (outputs as expected)
    
    However the model fails to fit, and outputs an error i have yet to figure out

--- Kaggle_MOdel/EfficientNetB4.py
+++ Kaggle_MOdel/EfficientNetB4.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.applications.efficientnet import EfficientNetB4
+from tensorflow.keras.applications import efficientnet
 import pandas as pd
 from PIL import Image
 import numpy as np
@@ -17,7 +17,7 @@ def loadData(csv_path):
     paths = df_csv["filepaths"]
     labels = df_csv["labels"]
     data_set = df_csv["data set"]
-    return df_csv
+    return df_csv, class_list, paths, labels, data_set
 
 
 def image_to_numpyarray(paths):
@@ -26,11 +26,12 @@ def image_to_numpyarray(paths):
     :return: list of numpy arrays, of the images
     """
     image_list = []
+
     for path in paths:
         image = Image.open(path)
         image_list.append(np.asarray(image))
         image.close()
-    return np.array(image_list)
+    return np.stack(image_list)
 
 
 # Use a custom callback function to navigate different Learning rates for the fit function
@@ -42,28 +43,32 @@ def scheduler(epoch, lr):
 
 
 if __name__ == "__main__":
-    df_csv = loadData("birds.csv")
+    df_csv, _, _, _, _ = loadData("birds.csv")
     df_train = df_csv[df_csv.values == "train"]
     df_test = df_csv[df_csv.values == "test"]
     df_val = df_csv[df_csv.values == "valid"]
-    print("Loaded data succesfully")
 
     paths_train = df_train["filepaths"]
     paths_test = df_test["filepaths"]
     paths_val = df_val["filepaths"]
+    print("Loaded data successfully")
 
-    model = EfficientNetB4(include_top=True, weights='imagenet')
+    model = efficientnet.EfficientNetB0(include_top=False,
+                                        weights='imagenet'
+                                        )
     callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
     model.compile(tf.keras.optimizers.SGD(), loss='mse')
-    print("Model assigned succesfully")
+    print("Model assigned successfully")
 
+    # TODO: make a function that saves the data so we dont have to convert all the image data everytime
     X_train = image_to_numpyarray(paths_train)
     Y_train = np.array(df_train["class index"])
     X_test = image_to_numpyarray(paths_test)
     Y_test = np.array(df_test["class index"])
     X_val = image_to_numpyarray(paths_val)
     Y_val = np.array(df_val["class index"])
-    print("Image data converted succesfully")
+    print("Image data converted successfully")
 
+    # TODO: figure out why the model doesn't want to fit
     model_fit = model.fit(X_train, Y_train, epochs=25, callbacks=[callback])
-    print(model_fit)
+    model_fit.summary()

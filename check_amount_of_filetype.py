64beef5371722d1ca22588c60d1eb40a95c3216c
Kris Kruse <kriskruse@hotmail.com>
2022-03-16 16:11:28 +0100

    more helper funktions

--- check_amount_of_filetype.py
+++ check_amount_of_filetype.py
@@ -5,3 +5,9 @@ lst = glob.glob("images_crop/*/*.JPG")
 lst2 = glob.glob("images_crop/*/*.XML")
 print(f"Amount of birds in images_crop: {len(lst)}")
 print(f"Amount of XML in images_crop: {len(lst2)}")
+
+lst3 = glob.glob("images/*/*.XML")
+print(f"Amount of XML in images: {len(lst3)}")
+
+lst4 = glob.glob("images2/*/*.XML")
+print(f"Amount of XML in images2: {len(lst4)}")

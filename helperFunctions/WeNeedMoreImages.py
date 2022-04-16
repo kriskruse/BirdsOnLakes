36fa94b6cfacc752d61169d0837beab64ea1c59c
Kris Kruse <kriskruse@hotmail.com>
2022-04-16 11:04:15 +0200

    WeNeedMoreImages is working
    so lets make a save of it

--- helperFunctions/WeNeedMoreImages.py
+++ helperFunctions/WeNeedMoreImages.py
@@ -4,6 +4,8 @@ import glob
 from PIL import Image
 import random
 import numpy as np
+import tqdm
+import threading
 
 
 class Images:
@@ -15,7 +17,7 @@ class Images:
         return self.lst[random.randrange(0, self.size, 1)]
 
 
-def createRandomImage(background, front, saveLocation, minAmount, maxAmount, path):
+def createRandomImage(front, background, saveLocation, minAmount, maxAmount, path):
     locations = []  # getting the location for later
     size = background.size
     # Add a random amount of birds
@@ -24,12 +26,12 @@ def createRandomImage(background, front, saveLocation, minAmount, maxAmount, pat
         y = random.randrange(int(size[1] * 0.2), int(size[1] * 0.80), 1)  # find a y position
         delta = random.randrange(500, 1000, 1) / 1000  # scale the bird so we have different sizes
         resize = [int(i) for i in np.multiply(front.size, delta)]  # scale the pixel size and convert to integer
-        front = front.resize(resize)  # reassign to the new scaled version
-        background.paste(front, (x, y), front)  # add bird to image
-        locations.append((x, y, delta))  # append its location
+        refront = front.resize(resize)  # reassign to the new scaled version
+        background.paste(refront, (x, y), refront)  # add bird to image
+        locations.append((x, y, resize))  # append its location
     filename = f"{path[6:-4]}{random.randrange(100000, 999999, 1)}"  # generate a random filename that we can save as
     background.save(f"{saveLocation}/{filename}.jpg")
-    return locations, filename  # [x, y, delta], "file"
+    return locations, filename  # [x, y, resize], "file"
 
 
 def getBoundingData(front, background, locations, labelpath, classlist, filename):
@@ -38,9 +40,8 @@ def getBoundingData(front, background, locations, labelpath, classlist, filename
     loclog = []
     with open(f"{labelpath}/{filename}.txt", 'w') as textfile:
         for location in locations:
-            x1, y1, delta = location
-            resize = [int(i) for i in np.multiply(front.size, delta)]
-            x2, y2 = np.add([x1, y1], resize)
+            x1, y1, size = location
+            x2, y2 = np.add([x1, y1], size)
 
             width = (x2 - x1) / xim
             height = (y2 - y1) / yim
@@ -49,7 +50,7 @@ def getBoundingData(front, background, locations, labelpath, classlist, filename
             classnum = classlist.index(class_name)
             textfile.write(f"{classnum} {xcenter} {ycenter} {width} {height}")
             textfile.write('\n')
-            loclog.append([x1, x2, y1, y2])
+            loclog.append([x1, y1, x2, y2])
 
     return loclog
 
@@ -77,23 +78,31 @@ def testBoundingBoxes(datapath, labelpath):
             centery = centery * imy
             width = width * imx
             height = height * imy
-            x1 = int(centerx - width / 2)
-            x2 = int(centerx + width / 2)
-            y1 = int(centery - height / 2)
-            y2 = int(centery + height / 2)
+            x1 = int(centerx - (width / 2))
+            x2 = int(centerx + (width / 2))
+            y1 = int(centery - (height / 2))
+            y2 = int(centery + (height / 2))
             cv2.rectangle(im, (x1, y1), (x2, y2), (255, 0, 0), 1)
-            locations.append([x1, x2, y1, y2])
+            locations.append([x1, y1, x2, y2])
         except:
             continue
 
     # Show the test image
-    cv2.imshow("show", im)
+    os.makedirs("BoundingTest", exist_ok=True)
+    cv2.imwrite(f"BoundingTest/{ranimage[-14:]}", im)
     cv2.waitKey()
     cv2.destroyAllWindows()
     return locations
 
 
 if __name__ == "__main__":
+
+    # This is for testing only
+    try:
+        shutil.rmtree("dataset")
+        shutil.rmtree("BoundingTest")
+    except:
+        print("Nothing to remove")
 
     ## Parameters
     birds = Images("birds/*.png")
@@ -102,7 +111,7 @@ if __name__ == "__main__":
     labelsavepath = "dataset/labels"
     minbirds = 3
     maxbirds = 4
-    N = 1
+    N = 10
     classlist = ['Duck', 'Cormorant', 'Heron;fa', 'Duck;fa', 'Goose', 'Cormorant;fa', 'Goose;fa', 'Swan;fa',
                  'Mute Swan', 'Seagull', 'Swan', 'Heron']
 
@@ -114,16 +123,14 @@ if __name__ == "__main__":
     os.makedirs(labelsavepath, exist_ok=True)
 
     # Runtime
-    for i in range(N):
+    for i in tqdm.tqdm(range(N), desc="Images to generate"):
         front_path = birds.random()
         background = backgrounds.random()
         with Image.open(front_path) as front:
             with Image.open(background) as background:
-                locations, filename = createRandomImage(background, front, savelocation, minbirds, maxbirds, front_path)
+                locations, filename = createRandomImage(front, background, savelocation, minbirds, maxbirds, front_path)
                 boundloc = getBoundingData(front, background, locations, labelsavepath, classlist, filename)
 
     # test a random image from path
-    for i in range(1):
+    for i in tqdm.tqdm(range(int(N / 100 if not N/100 <1 else 1)), desc="Running boundingBox check"):
         testloc = testBoundingBoxes("dataset/images/*.jpg", "dataset/labels")
-        print(boundloc)
-        print(testloc)
